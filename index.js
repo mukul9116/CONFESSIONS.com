1ae5a934ad95e2cc24a1083ff8dd223f7bf6d183
Mukul Jangid <mukuljd91@gmail.com>
2025-08-23 01:50:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
